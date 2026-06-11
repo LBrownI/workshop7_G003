d448919b1dc841172a65e35cdce9ebcf8668b871
Angelo Vargas <avargasc9@correo.uss.cl>
2026-06-11 18:32:54 -0400

    Entrega Taller 7 - Angelo Vargas

---
+++ Angelo_Vargas/models/user_schema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const conectarDB = async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/taller7_db');
+        console.log('Conexión a MongoDB exitosa');
+    } catch (err) {
+        console.log('Fallo al conectar a MongoDB', err);
+    }
+};
+conectarDB();
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
