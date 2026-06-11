a305c6a0300d9d9f794d1c41754a88eb4539f108
mathias <mcarrerah@correo.uss.cl>
2026-06-11 13:01:50 -0400

    Entrega Taller 7 API y JWT

---
+++ mathias_carrera/routes/api.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+const { User } = require('../models/user_schema');
+
+const authorize = require('../controllers/auth_controller');
+
+const SECRET_KEY = 'your-super-secret';
+
+// TAREA 2: Implementar Operación GET
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// TAREA 3 y 5: Implementar Operación POST y proteger con middleware
+router.post('/users', authorize(['admin']), async (req, res) => {
+    try {
+        const { name, email } = req.body;
+        
+        const newUser = new User({ name, email });
+        
+        const savedUser = await newUser.save();
+        
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// TAREA 4: Generación de Token (Login)
+router.post('/login', async (req, res) => {
+    try {
+        
+        const payload = {
+            usuario: 'test_user',
+            role: 'admin'
+        };
+
+        const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+
+        res.status(200).json({ token: token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
